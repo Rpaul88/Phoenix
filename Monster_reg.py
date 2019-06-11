abfd83d09fb53e23fc4c99c5eb997ed769acb6b7
Your Name <you@example.com>
2019-06-11 11:55:29 +0530

    Initial commit

---
+++ Monster_reg.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver=webdriver.Chrome(executable_path="C:/WebDrivers/chromedriver")
+driver.get("https://www.monsterindia.com/seeker/registration")
+driver.implicitly_wait(20)
+driver.find_element_by_xpath("//*[@class='box tc']").click()
+driver.find_element_by_name("fullname").send_keys("Rajesh")
+driver.find_element_by_name("email").send_keys("abc@123")
+driver.find_element_by_xpath("//*[@class='input pr30']").send_keys("123456")
+
+driver.find_element_by_xpath("//div[contains(@class,'multiselect modal-ref-class multiBox')]").click()
+driver.find_element_by_xpath("//input[contains(@class,'multiselect__input')]").send_keys("Thr")
+time.sleep(2)
+driver.find_element_by_xpath("//input[contains(@class,'multiselect__input')]").send_keys(Keys.ENTER)
---
+++ SeleniumBasics.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+ch_browser=webdriver.Chrome(executable_path="C:/chromedriver_win32/chromedriver")
+ch_browser.get("http://facebook.com")
